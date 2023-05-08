af77e2a0ebab2d3f433411838b285bcb255da54f
IK62 <ismail.karrouch13@gmail.com>
2023-05-08 12:51:59 +0100

    change homepage

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -1,7 +1,29 @@
 function home() {
-    return ( 
-        <h1>homepage</h1>
-     )
+  return (
+    <div className="flex flex-col p-36 gap-y-10">
+      <p className="text-7xl">
+        Bienvenue sur{' '}
+        <a className="text-transparent bg-clip-text bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-indigo-400 via-slate-700 to-zinc-800 bg-[length:500%_500%]">
+          team list
+        </a>
+      </p>
+      <p className="text-4xl">
+        sur ce site vous pouvez{' '}
+        <a className="text-transparent bg-clip-text bg-gradient-to-r from-violet-600 to-indigo-600">
+          crée
+        </a>
+        ,{' '}
+        <a className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-purple-900">
+          supprimer
+        </a>{' '}
+        et{' '}
+        <a className="text-transparent bg-clip-text bg-gradient-to-r from-blue-800 to-indigo-900">
+          modifier
+        </a>{' '}
+        vos équipes autant que vous le voulez !
+      </p>
+    </div>
+  )
 }
 
-export default home;
+export default home
--- src/components/root.jsx
+++ src/components/root.jsx
@@ -3,14 +3,14 @@ import { Link, Outlet } from "react-router-dom";
 export default function Root() {
     return (
         <>
-            <nav>
-                <Link to={"home"}>
+            <nav className="flex justify-center py-10 gap-x-16">
+                <Link to={"/"} className="nav-text">
                     home
                 </Link>
-                <Link to={"list"}>
+                <Link to={"list"} className="nav-text">
                     create your teams
                 </Link>
-                <Link to={"contact"}>
+                <Link to={"contact"} className="nav-text">
                     contact
                 </Link>
             </nav>
